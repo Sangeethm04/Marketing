cb2bce066caa61c8baed99c33ebfdf7e11373d71
Sangeeth Menachery <43912785+Sangeethm04@users.noreply.github.com>
2024-04-18 13:25:30 -0400

    ev

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import requests
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+#wait for the page to load
+from selenium.webdriver.support.ui import WebDriverWait
 
 API_KEY = "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"
 
@@ -13,7 +15,7 @@ def get_response(message):
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=[
-            {"role": "user", "content": "find the most relevant owner/ceo of the company from the given text-just give the name no other information: " + ' '.join(message)},
+            {"role": "user", "content": "find the most relevant owner/ceo of the company from the given text-just give the name no other information. If it doesn't explicitly say owner or ceo currently then just return with n/a: " + ' '.join(message)},
         ]
     )
     print(response)
@@ -25,25 +27,31 @@ def pull_google_search(company):
 
     # driver = webdriver.Chrome(options=chrome_options)
 
-    searchquery = company + " owner "
+    searchquery = company + " owner/ceo "
     # driver.get("https://www.google.com/search?q=" + searchquery)
     soup = BeautifulSoup(requests.get("https://www.google.com/search?q=" + searchquery).text, "html.parser")
-    #print(soup.prettify())
+
     #find tags with the xpath
-    mydivs = soup.find_all("div", xpath='//*[@id="rso"]/div[3]/div/div/div[2]/div/span/text()')
+    #mydivs = soup.find_all("div", xpath='//*[@id="rso"]/div[3]/div/div/div[2]/div/span/text()')
     allLines = []
-    for line in soup.find_all('span'):
-        print(line.get_text())
-        #store results in variable and return it
+    neededLines = []
+    #find all divs
+    for line in soup.find_all('div'):
         allLines.append(line.get_text())
-    return allLines
+
+    #get the first 5...50 lines
+    for line in range(0, 50):
+        neededLines.append(allLines[line])
+    return neededLines
     
 
 
 #main method
 if __name__ == "__main__":
-    
-    messages = pull_google_search("Zaveta Custom Homes")
+
+    company_name = "Timmons Construction"
+
+    messages = pull_google_search(company_name)
     print(messages)
     get_response(messages)
 
